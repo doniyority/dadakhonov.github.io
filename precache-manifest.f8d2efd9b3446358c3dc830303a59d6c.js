c9588987db53cccd15451175b8543f9f4cbc0ad1
Doniyor Dadakhonov <doniyor-dadaxonov@mail.ru>
2019-03-01 09:51:15 +0500

    first commit

---
+++ precache-manifest.f8d2efd9b3446358c3dc830303a59d6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f6b55cd4e8702a5d49eb",
+    "url": "/static/js/main.f6b55cd4.chunk.js"
+  },
+  {
+    "revision": "c153991f913cad1f9786",
+    "url": "/static/js/2.c153991f.chunk.js"
+  },
+  {
+    "revision": "f6b55cd4e8702a5d49eb",
+    "url": "/static/css/main.c7d285f1.chunk.css"
+  },
+  {
+    "revision": "405788af13cacc6a4b2094d367525b7e",
+    "url": "/index.html"
+  }
+];
